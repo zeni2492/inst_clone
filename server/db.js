e11b5e9f453c07edef210987742cc4484ff84cdb
zeni2492 <mr.sher123@mail.ru>
2025-01-08 13:20:52 +0700

    added env file

--- server/db.js
+++ server/db.js
@@ -2,13 +2,13 @@ const Pool = require("pg").Pool; // lib for connecting to database
 
 const pool = new Pool({
     //data for connection to database
-    label: "instagram",
-    host: "localhost",
-    user: "postgres",
+    label: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
     port: 5432,
     ssl: false,
-    database: "instagram",
-    password: "vlad3003",
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
 });
 
 module.exports = pool;
